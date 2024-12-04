b17e0496e3c3ffa5f354165cd6837997400115f4
lubenwei <819577544@qq.com>
2024-12-04 11:40:57 +0800

    11

--- huojiweiguoba/lbw_txyc.py
+++ huojiweiguoba/lbw_txyc.py
@@ -9,6 +9,7 @@ import copy
 import os.path
 import inspect
 import traceback
+import datetime
 
 import peewee
 from decimal import Decimal
@@ -74,10 +75,14 @@ class BaseModel(peewee.Model):
     class Meta:
         database = db
 
-    def __init_subclass__(cls, **kwargs):
-        super().__init_subclass__(**kwargs)
-        if cls.unique_keys:
-            cls._meta.indexes = [(tuple(cls.unique_keys), True)]
+    # def __init_subclass__(cls, **kwargs):
+    #     super().__init_subclass__(**kwargs)
+    #     # 建立联合唯一索引
+    #     if cls.unique_keys:
+    #         cls._meta.indexes = [(tuple(cls.unique_keys), True)]
+
+    def __str__(self):
+        return str(self.__idata__)
 
 class Shops(BaseModel):
     __table_name__ = 'shops'
@@ -88,30 +93,43 @@ class Shops(BaseModel):
     shop_notes = peewee.CharField()
     is_open = peewee.BooleanField(default=1)
 
+    class Meta:
+        indexes = (
+            (("shop_name", "is_open"), True),
+        )
 class JdGoods(BaseModel):
     __table_name__ = 'jd_goods'
     unique_keys = ['goods_authorized_no', 'goods_spec']
     goods_cost = peewee.DecimalField(decimal_places=2, max_digits=10, verbose_name='商品成本价')
+    goods_brand = peewee.CharField(null=False, verbose_name='品牌')
+    goods_genericName = peewee.CharField(null=False, verbose_name='药品通用名')
+    goods_manufacturer = peewee.CharField(null=False, verbose_name='生产厂家')
     goods_authorized_no = peewee.CharField(null=False,verbose_name='批准文号')
     goods_spec = peewee.CharField(null=False,verbose_name='规格')
     goods_category = peewee.CharField(null=False,verbose_name='类目')
     goods_code = peewee.CharField(null=False,verbose_name='商品代码')
     goods_limit_address = peewee.CharField(null=False, verbose_name='限购地址')
-    goods_notes = peewee.CharField(null=False, verbose_name='备注')
-    goods_images_local_path = peewee.TextField(null=False, verbose_name='图片本地路径')
+    goods_notes = peewee.CharField(null=True, verbose_name='备注')
+    goods_images_local_path = peewee.TextField(null=True, verbose_name='图片本地路径')
     goods_enabled = peewee.BooleanField(default=1, verbose_name='是否启用')
 
+    class Meta:
+        indexes = (
+            (("goods_authorized_no", "goods_spec"), True),
+        )
 
 
 class JdListing(BaseModel):
     __table_name__ = 'jd_listing'
-    unique_keys = ['shop_id', 'goods_id']
+    unique_keys = ['shop_id', 'goods_id','create_index']
     shop_id = peewee.IntegerField()
     goods_id = peewee.IntegerField()
-    create_date_time = peewee.DateTimeField(null=False,verbose_name='创建时间')
+    create_index = peewee.IntegerField(null=False,verbose_name='创建索引',default=1)
+    create_date_time = peewee.DateTimeField(null=False,verbose_name='创建时间',default=datetime.datetime.now)
     done_date_time = peewee.DateTimeField(null=True,verbose_name='完成时间')
     status = peewee.CharField(null=False,verbose_name='状态')
-    done_babyid = peewee.IntegerField(null=True,verbose_name='宝贝ID')
+    done_msg = peewee.TextField(null=True, verbose_name='上架消息')
+    done_babyid = peewee.CharField(null=True,verbose_name='宝贝ID')
     done_limit_address_msg = peewee.TextField(null=True, verbose_name='限购地址-消息')
     done_shop_promotion_msg = peewee.TextField(null=True, verbose_name='店铺促销-消息')
     done_image_template_msg = peewee.TextField(null=True, verbose_name='图片模板-消息')
@@ -121,6 +139,11 @@ class JdListing(BaseModel):
     done_goods_common_name = peewee.CharField(null=True, verbose_name='商品通用名')
     done_goods_brand = peewee.CharField(null=True, verbose_name='品牌')
     done_goods_69code = peewee.CharField(null=True, verbose_name='69码')
+
+    class Meta:
+        indexes = (
+            (("shop_id", "goods_id", "create_index"), True),
+        )
 
 db.connect()
 db.create_tables([Shops], safe=True)
